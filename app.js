19dcf8a525feaf432ec8720efb1ed823e0b57663
Andrés Angulo <andresangulo90@gmail.com>
2025-09-29 16:49:31 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+const cors = require('cors');
+const corsOptions = {
+    origin: ['https://sio2renovations.com/', 'http://140.0.7339.208'],
+    credentials: true
+};
+app.use(cors(corsOptions));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
