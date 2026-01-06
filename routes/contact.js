54388822612ec33f12061df233012f03af7316a0
Andrés Angulo <andresangulo90@gmail.com>
2026-01-06 20:12:31 +0100

    fix: attempt resolve issue with the second form submission following

--- routes/contact.js
+++ routes/contact.js
@@ -7,6 +7,16 @@ const { handleCustomerContactForm } = require('../controllers/customerContactCon
 
 router.post('/send_partner_email', handlePartnerContactForm);
 
-router.post('/send_customer_email', upload.array('files'), handleCustomerContactForm);
+// router.post('/send_customer_email', upload.array('files'), handleCustomerContactForm);
+
+router.post('/send_customer_email', upload.array('files'), (req, res, next) => {
+  console.log('--- /contact/send_customer_email ---');
+  console.log('Time:', new Date().toISOString());
+  console.log('Headers:', req.headers);
+  console.log('Body keys:', Object.keys(req.body || {}));
+  console.log('Files count:', req.files ? req.files.length : 0);
+
+  return handleCustomerContactForm(req, res, next);
+});
 
 module.exports = router;
