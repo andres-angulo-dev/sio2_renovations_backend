ba439b400462b4d8bf88ee249213379fa3256ec5
Andrés Angulo <andresangulo90@gmail.com>
2026-01-06 20:12:31 +0100

    fix: attempt resolve issue with the second form submission following

--- controllers/customerContactController.js
+++ controllers/customerContactController.js
@@ -2,9 +2,10 @@ const { sendCustomerContactEmail, sendAcknowledgmentEmail } = require('../servic
 const { checkBody } = require('../modules/checkBody');
 
 exports.handleCustomerContactForm = async (req, res) => {
+    console.log('[handleCustomerContactForm] Controller reached');
+
     if (!checkBody(req.body, ['lastName', 'email', 'message', 'requestType'])) {
         res.json({ result: false, error: 'Missing or empty fields'});
-        print('[Backend]: CustomerContactController : T ICI');
         return;
     }
 
