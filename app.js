62ffa85d948de9b9ed2cfb5b561fa069cbb76a4b
Andrés Angulo <andresangulo90@gmail.com>
2026-01-09 00:19:14 +0100

    chore: implement secure dynamic CORS rules (web, mobile, prodcution and dev)

--- app.js
+++ app.js
@@ -11,12 +11,25 @@ var contactRouter = require('./routes/contact');
 
 var app = express();
 const cors = require('cors');
-// const corsOptions = {
-//     origin: ['https://sio2renovations.com/', 'http://140.0.7339.208'],
-//     credentials: true
-// };
-// app.use(cors(corsOptions));
-app.use(cors());
+const allowedOrigins = [ // Production 
+    'https://sio2renovations.com', 
+    'https://www.sio2renovations.com', 
+];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true); // Allows requests without origin (mobile apps, curl, Postman)
+
+        if (origin.startsWith('http://localhost') || origin.startsWith('http://127.0.0.1')) // Allows localhost and 127.0.0.1 dynamically (http://localhost:50789; http://localhost:5173; http://127.0.0.1:8080)
+            return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) // Allows production origins
+            return callback(null, true);
+
+        return callback(new Error('Not allowed by CORS'));
+    },
+    credentials: true // Allows the browser to send cookies, tokens or auth headers.
+};
+app.use(cors(corsOptions));
 
 app.use(logger('dev'));
 app.use(express.json({ limit: '20mb' }));
